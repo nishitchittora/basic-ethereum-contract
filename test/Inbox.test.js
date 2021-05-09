8454e9168e401d11f23857d99f8189913bc32616
nishitchittora <nishitchittora2015@gmail.com>
2021-05-09 14:22:00 +0530

    basic contract deployed

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,27 +1,29 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
-
 const web3 = new Web3(ganache.provider());
-
+const {interface, bytecode} = require("../compile");
 
 // only deploy a contract when we have access to external account
-
-beforeEach(()=>{
+let accounts, inbox;
+beforeEach(async ()=>{
     // Get a list of Unlock accounts from ganache
-    web3.eth.getAccounts().then(fetchAccounts=>{
-        console.log(fetchAccounts)
-    })
+    accounts = await web3.eth.getAccounts()
 
 
     // Use one of those account to deploy contract
-
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+        data: bytecode,
+        arguments: ["Hi there!"]
+    })
+    .send({from: accounts[0], gas: '1000000'});
 
 });
 
 
 describe("Inbox", ()=>{
     it("deploy a contract", ()=>{
-
+      console.log(inbox);
     });
 })
