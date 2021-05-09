fc045fae3d68422a353ba6b938bd958f53688941
nishitchittora <nishitchittora2015@gmail.com>
2021-05-09 16:43:14 +0530

    deploy contract at rinkeby network

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,7 +5,7 @@ const web3 = new Web3(ganache.provider());
 const {interface, bytecode} = require("../compile");
 
 // only deploy a contract when we have access to external account
-let accounts, inbox;
+let accounts, inbox, INITIAL_STRING="Hi there";
 beforeEach(async ()=>{
     // Get a list of Unlock accounts from ganache
     accounts = await web3.eth.getAccounts()
@@ -15,15 +15,29 @@ beforeEach(async ()=>{
     inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
         data: bytecode,
-        arguments: ["Hi there!"]
-    })
-    .send({from: accounts[0], gas: '1000000'});
+        arguments: [INITIAL_STRING]
+    }).send({from: accounts[0], gas: '1000000'});
 
 });
 
 
 describe("Inbox", ()=>{
     it("deploy a contract", ()=>{
-          assert.ok(inbox.options.address);
+        assert.ok(inbox.options.address);
     });
+
+    it("Default value for constructor", async()=>{
+          const message = await inbox.methods.message().call();
+          assert.equal(message, INITIAL_STRING)
+    });
+
+    it("can change the message", async()=>{
+        let t =await inbox.methods.setMessage("Bye there!").send({
+            from: accounts[0]
+        })
+        console.log(t);
+        const message = await inbox.methods.message().call();
+        assert.equal(message, "Bye there!");
+    });
+
 })
